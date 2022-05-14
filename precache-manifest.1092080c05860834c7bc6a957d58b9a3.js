565be9e7449b7357cf02c0a103646b1a2b2ac39a
xiang510 <vip_319@126.com>
2022-05-14 11:40:31 +0800

    initial

---
+++ precache-manifest.1092080c05860834c7bc6a957d58b9a3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "811e18d29e68c1e6e941f5252d8267a1",
+    "url": "/fonts/roboto-v18-latin_latin-ext-italic.woff2"
+  },
+  {
+    "revision": "3f61bd10f9c40ac41c871d619abd0c45",
+    "url": "/data/default.json"
+  },
+  {
+    "revision": "3d95a35efa84ecbee1cd5f964e963d79",
+    "url": "/img/japanese-phrasebook-screens.png"
+  },
+  {
+    "revision": "962e945fa1697fbbcc52",
+    "url": "/js/chunk-vendors.db34c7c0.js"
+  },
+  {
+    "revision": "bd0365532fc48ab73c0cc35d08f3a4a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d6a47a7c5844ab1b18b714254bfd6e1",
+    "url": "/img/kinkaku-ji.jpg"
+  },
+  {
+    "revision": "62f98d9931df521ef8b198f64c239877",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8959d0891f47c2561876377385d52ac6",
+    "url": "/shell.html"
+  },
+  {
+    "revision": "056e4dddab48f4a8f9c6ac9622e64d23",
+    "url": "/sitemap.xml"
+  },
+  {
+    "revision": "89044585f8804da1ed89551178cc7c8f",
+    "url": "/fonts/roboto-v18-latin_latin-ext-500.woff2"
+  },
+  {
+    "revision": "53eedb1e2d7c79582f367ec102c12681",
+    "url": "/fonts/roboto-v18-latin_latin-ext-700.woff2"
+  },
+  {
+    "revision": "763e323434259416ec12",
+    "url": "/js/app.567df121.js"
+  },
+  {
+    "revision": "e5d046dd2c38f8e9a1f2f1130e4ec9fc",
+    "url": "/fonts/roboto-v18-latin_latin-ext-regular.woff2"
+  },
+  {
+    "revision": "d14ff5136e64dbb064089c93c8925904",
+    "url": "/fonts/roboto-v18-latin_latin-ext-700.woff"
+  },
+  {
+    "revision": "cc04fb84dbdbd997be1b8b7ae687fb3c",
+    "url": "/fonts/roboto-v18-latin_latin-ext-500.woff"
+  },
+  {
+    "revision": "1fb51f52c5d3adf198aa44e80478426e",
+    "url": "/fonts/roboto-v18-latin_latin-ext-regular.woff"
+  },
+  {
+    "revision": "882f0af85f688d84afb3ced39c3e9d28",
+    "url": "/fonts/roboto-v18-latin_latin-ext-italic.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "962e945fa1697fbbcc52",
+    "url": "/css/chunk-vendors.80a9e6ca.css"
+  },
+  {
+    "revision": "763e323434259416ec12",
+    "url": "/css/app.30ace0a8.css"
+  },
+  {
+    "revision": "246112a111faaa4b348de7a99f544344",
+    "url": "/_redirects"
+  }
+];
